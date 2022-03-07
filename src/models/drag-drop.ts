19cd93006a57631c5a12e45f5b3e8b1329fe7a0f
quyentrankhanhha <t9qutr00@students.oamk.fi>
2022-03-07 17:04:33 +0200

    done section 10

---
+++ src/models/drag-drop.ts
@@ -0,0 +1,11 @@
+// drag and drop interfaces
+export interface Draggable {
+  dragStartHandler(event: DragEvent): void
+  dragEndHandler(event: DragEvent): void
+}
+
+export interface DragTarget {
+  dragOverHandler(event: DragEvent): void
+  dropHandler(event: DragEvent): void
+  dragLeaveHandler(event: DragEvent): void
+}
